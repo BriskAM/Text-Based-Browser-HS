362cf9fca15c2689dbe205fbdb4e8ca87a2d41fe
Akshit Mehta <mehtaakshit1695@gmail.com>
2024-04-11 13:51:56 +0530

    Implement backing

--- Text-Based Browser/task/browser.py
+++ Text-Based Browser/task/browser.py
@@ -1,4 +1,5 @@
 import argparse
+import collections
 import os
 
 parser = argparse.ArgumentParser(description="This program is a text based browser in python.")
@@ -44,14 +45,23 @@ Twitter and Square Chief Executive Officer Jack Dorsey
 
 dns = {"nytimes.com": nytimes_com, "bloomberg.com": bloomberg_com}
 open_tabs = []
+current_tab = collections.deque()
 for url in iter(input, 'exit'):
     if url in dns:
         print(dns.get(url.strip()))
         with open(os.path.join(args.directory, str(url.strip().split(".")[0])), 'w') as t:
             t.write(dns.get(url))
         open_tabs.append(str(url.split(".")[0]))
+        current_tab.append(url)
+
     elif url in open_tabs:
         with open(os.path.join(args.directory, url), 'r') as t:
             print(t.read())
+    elif url == 'back':
+        try:
+            current_tab.pop()
+            print(dns.get(current_tab[-1].strip()))
+        except IndexError:
+            pass
     else:
         print("Error: Invalid URL")
